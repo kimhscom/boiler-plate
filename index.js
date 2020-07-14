7e93b013ae6ef49f50491e9e15689fba10bbe182
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-07-14 19:29:32 +0900

    #7 BodyParser & PostMan & SignUp function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import User from "./models/User";
 dotenv.config();
 
 const app = express();
@@ -16,7 +18,25 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  // If you get the information you need to sign up from the client,
+  // Put them in the database.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
