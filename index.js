d96ac837f351119c8b5a67ee028da9d3479c8eae
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-07-14 19:54:55 +0900

    #9 Secret Settings Information Management

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ const app = express();
 const PORT = 5000;
 
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
+  .connect(
+    process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
