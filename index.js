96a5240576ba349c71bc7ddda0097b84a7a7612c
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-07-14 17:32:17 +0900

    #3 MongoDB connection

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(PORT, () =>
